eeed1f121aec16d6a1ac101942bc41fd0f17ac51
Rajat Badjatya <rajatbadjatya@gmail.com>
2018-08-19 19:24:48 +0530

    handle  statusdialog invokation using triggeraction

--- app.js
+++ app.js
@@ -34,35 +34,75 @@ var questions = [
   { field: 'tTask', prompt: 'What you are planning to do today?' },
   { field: 'blocker', prompt: 'Any blocker?' }
 ];
-// This bot ensures user's profile is up to date.
-var bot = new builder.UniversalBot(connector, [
-  function(session) {
-    console.log(moment(new Date()).format('DD-MM-YYYY'));
-    session.beginDialog('statusDialog');
-  },
-  function(session, results) {
-    session.send('Conclusion');
-    console.log(JSON.stringify(results.response));
-    var tasksRef = dB.ref().child('tasks');
-    //var objToSend = [];
-    for (i = 0; i < users.length; i++) {
-      var user = users[i];
-      var obj = {};
-      var taskId = `${projectId}_${moment(new Date()).format('DD-MM-YYYY')}_${user.id}`
-      obj = {
-        yTask : results.response[taskId].yTask,
-        tTask: results.response[taskId].tTask,
-        blocker: results.response[taskId].blocker
-      };
-      tasksRef.child(taskId).set(obj);
-      session.send(
-        `${users[i].name}: <br> ${results.response[taskId].yTask} <br> ${
-          results.response[taskId].tTask
-        } <br> ${results.response[taskId].blocker}`
-      );
+
+
+// Create your bot with a function to receive messages from the user
+// This default message handler is invoked if the user's utterance doesn't
+// match any intents handled by other dialogs.
+var bot = new builder.UniversalBot(connector, function (session, args) {
+  session.send('You reached the default message handler. You said \'%s\'.', session.message.text);
+}).set('storage', inMemoryStorage);
+
+bot.dialog('InitialDialog',[
+    (session) => {
+        if(session.message.user.name === 'rajatbadjatya')
+            session.beginDialog('statusDialog');
+        else
+          session.send('You are not an authorized user to start this standup');
     }
+],(session, results) => {
+  session.send('Conclusion');
+  console.log(JSON.stringify(results.response));
+  var tasksRef = dB.ref().child('tasks');
+  //var objToSend = [];
+  for (i = 0; i < users.length; i++) {
+    var user = users[i];
+    var obj = {};
+    var taskId = `${projectId}_${moment(new Date()).format('DD-MM-YYYY')}_${user.id}`
+    obj = {
+      yTask : results.response[taskId].yTask,
+      tTask: results.response[taskId].tTask,
+      blocker: results.response[taskId].blocker
+    };
+    tasksRef.child(taskId).set(obj);
+    session.send(
+      `${users[i].name}: <br> ${results.response[taskId].yTask} <br> ${
+        results.response[taskId].tTask
+      } <br> ${results.response[taskId].blocker}`
+    );
   }
-]).set('storage', inMemoryStorage); // Register in-memory storage
+}).triggerAction({
+  matches: /^Start$/i
+});
+// // This bot ensures user's profile is up to date.
+// var bot = new builder.UniversalBot(connector, [
+//   function(session) {
+//     console.log(moment(new Date()).format('DD-MM-YYYY'));
+//     session.beginDialog('statusDialog');
+//   },
+//   function(session, results) {
+//     session.send('Conclusion');
+//     console.log(JSON.stringify(results.response));
+//     var tasksRef = dB.ref().child('tasks');
+//     //var objToSend = [];
+//     for (i = 0; i < users.length; i++) {
+//       var user = users[i];
+//       var obj = {};
+//       var taskId = `${projectId}_${moment(new Date()).format('DD-MM-YYYY')}_${user.id}`
+//       obj = {
+//         yTask : results.response[taskId].yTask,
+//         tTask: results.response[taskId].tTask,
+//         blocker: results.response[taskId].blocker
+//       };
+//       tasksRef.child(taskId).set(obj);
+//       session.send(
+//         `${users[i].name}: <br> ${results.response[taskId].yTask} <br> ${
+//           results.response[taskId].tTask
+//         } <br> ${results.response[taskId].blocker}`
+//       );
+//     }
+//   }
+// ]).set('storage', inMemoryStorage); // Register in-memory storage
 
 bot.dialog('statusDialog', [
   function(session, args, next) {
