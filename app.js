afbf9807f9853f21b1c4c83778e7b946c98ab220
Rajat Badjatya <rajatbadjatya@gmail.com>
2018-09-01 14:59:14 +0530

    added node schedule

--- app.js
+++ app.js
@@ -2,153 +2,94 @@ var firebase = require('firebase-admin');
 var restify = require('restify');
 var builder = require('botbuilder');
 var moment = require('moment');
+var schedule = require('node-schedule');
+
 // Setup Restify Server
 var server = restify.createServer();
-server.listen(process.env.port || process.env.PORT || 9100, function() {
+server.listen(process.env.port || process.env.PORT || 3978, function() {
   console.log('%s listening to %s', server.name, server.url);
 });
+
 //Setup and configure firebase
 var serviceAccount = require('./standup-bot-1-firebase-adminsdk');
-
 firebase.initializeApp({
   credential: firebase.credential.cert(serviceAccount),
   databaseURL: 'https://standup-bot-1.firebaseio.com'
 });
-
 //Database reference
 var dB = firebase.database();
+
 // Create chat connector for communicating with the Bot Framework Service
 var connector = new builder.ChatConnector({
   appId: process.env.MicrosoftAppId,
   appPassword: process.env.MicrosoftAppPassword
 });
+
 // Listen for messages from users
 server.post('/api/messages', connector.listen());
+
 // Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
 var inMemoryStorage = new builder.MemoryBotStorage();
 var statusList = [];
 var projectId = 'p_101';
-var users = [{ name: 'Rajat', id: '101' }, { name: 'Dhruv', id: '102' }];
+var user = {
+  name: 'Rajat',
+  id: '101'
+};
 var questions = [
-  { field: 'yTask', prompt: 'What did you do yesterday?' },
-  { field: 'tTask', prompt: 'What you are planning to do today?' },
-  { field: 'blocker', prompt: 'Any blocker?' }
+  {
+    field: 'yTask',
+    prompt: 'What did you do yesterday?'
+  },
+  {
+    field: 'tTask',
+    prompt: 'What you are planning to do today?'
+  },
+  {
+    field: 'blocker',
+    prompt: 'Any blocker?'
+  }
 ];
 
+function startProactiveDialog(address) {
+  console.log('==============>inside start Proactive dialog<==============');
+  // bot.endDailog();
+  bot.beginDialog(address, 'statusDialog');
+}
 // Create your bot with a function to receive messages from the user
 // This default message handler is invoked if the user's utterance doesn't
 // match any intents handled by other dialogs.
 var bot = new builder.UniversalBot(connector, [
   function(session) {
-    if (session.message.text.includes('Start')) {
-      if (session.message.user.name === 'rajatbadjatya') {
-        console.log('default flow');
-        session.beginDialog('statusDialog');
-      } else {
-        session.send(
-          'You are not an authorized user to start this standup',
-          session.message.text
-        );
-      }
-    }else {
-      // session.endDialog();
-    }
-  },
-  (session, results) => {
-    session.send('Conclusion');
-    console.log(JSON.stringify(results.response));
-    var tasksRef = dB.ref().child('tasks');
-    //var objToSend = [];
-    for (i = 0; i < users.length; i++) {
-      var user = users[i];
-      var obj = {};
-      var taskId = `${projectId}_${moment(new Date()).format('DD-MM-YYYY')}_${
-        user.id
-      }`;
-      obj = {
-        yTask: results.response[taskId].yTask,
-        tTask: results.response[taskId].tTask,
-        blocker: results.response[taskId].blocker
-      };
-      tasksRef.child(taskId).set(obj);
-      session.send(
-        `${users[i].name}: <br> ${results.response[taskId].yTask} <br> ${
-          results.response[taskId].tTask
-        } <br> ${results.response[taskId].blocker}`
-      );
-    }
+    // initiate a dialog proactively
+    savedAddress = session.message.address;
+    var message =
+      "Hello, it's time to start the daily standup. Please answer the following questions:";
+    session.send(message);
+    var rule = new schedule.RecurrenceRule();
+    rule.second = 10;
+
+    var j = schedule.scheduleJob(rule, function() {
+      console.log('-----------123-------------');
+      startProactiveDialog(savedAddress);
+    });
   }
 ]).set('storage', inMemoryStorage);
 
-// bot
-//   .dialog('InitialDialo', [
-//     session => {
-//       if (session.message.user.name === 'User')
-//         session.beginDialog('statusDialog');
-//       else session.send('You are not an authorized user to start this standup');
-//     },
-//     (session, results) => {
-//       session.send('Conclusion');
-//       console.log(JSON.stringify(results.response));
-//       var tasksRef = dB.ref().child('tasks');
-//       //var objToSend = [];
-//       for (i = 0; i < users.length; i++) {
-//         var user = users[i];
-//         var obj = {};
-//         var taskId = `${projectId}_${moment(new Date()).format('DD-MM-YYYY')}_${
-//           user.id
-//         }`;
-//         obj = {
-//           yTask: results.response[taskId].yTask,
-//           tTask: results.response[taskId].tTask,
-//           blocker: results.response[taskId].blocker
-//         };
-//         tasksRef.child(taskId).set(obj);
-//         session.send(
-//           `${users[i].name}: <br> ${results.response[taskId].yTask} <br> ${
-//             results.response[taskId].tTask
-//           } <br> ${results.response[taskId].blocker}`
-//         );
-//       }
-//     }
-//   ])
-//   .triggerAction({
-//     matches: /^Start$/i
-//   });
-// // This bot ensures user's profile is up to date.
-// var bot = new builder.UniversalBot(connector, [
-//   function(session) {
-//     console.log(moment(new Date()).format('DD-MM-YYYY'));
-//     session.beginDialog('statusDialog');
-//   },
-//   function(session, results) {
-//     session.send('Conclusion');
-//     console.log(JSON.stringify(results.response));
-//     var tasksRef = dB.ref().child('tasks');
-//     //var objToSend = [];
-//     for (i = 0; i < users.length; i++) {
-//       var user = users[i];
-//       var obj = {};
-//       var taskId = `${projectId}_${moment(new Date()).format('DD-MM-YYYY')}_${user.id}`
-//       obj = {
-//         yTask : results.response[taskId].yTask,
-//         tTask: results.response[taskId].tTask,
-//         blocker: results.response[taskId].blocker
-//       };
-//       tasksRef.child(taskId).set(obj);
-//       session.send(
-//         `${users[i].name}: <br> ${results.response[taskId].yTask} <br> ${
-//           results.response[taskId].tTask
-//         } <br> ${results.response[taskId].blocker}`
-//       );
-//     }
-//   }
-// ]).set('storage', inMemoryStorage); // Register in-memory storage
+// Send welcome when conversation with bot is started, by initiating the root dialog
+bot.on('conversationUpdate', function(message) {
+  if (message.membersAdded) {
+    message.membersAdded.forEach(function(identity) {
+      if (identity.id === message.address.bot.id) {
+        console.log('inside conversation update');
+        bot.beginDialog(message.address, '/');
+      }
+    });
+  }
+});
 
 bot.dialog('statusDialog', [
-  function(session, args, next) {
-    // Save previous state (create on first call)
-    session.dialogData.userIndex = args ? args.userIndex : 0;
+  (session, args, next) => {
     session.dialogData.questionIndex = args ? args.questionIndex : 0;
     session.dialogData.statusList = args ? args.statusList : {};
     session.dialogData.status = args ? args.status : {};
@@ -159,26 +100,42 @@ bot.dialog('statusDialog', [
       }`
     );
   },
-  function(session, results) {
+  (session, results) => {
     // Save users reply
     var field = questions[session.dialogData.questionIndex++].field;
     session.dialogData.status[field] = results.response;
-    var user = users[session.dialogData.userIndex];
     var taskId = `${projectId}_${moment(new Date()).format('DD-MM-YYYY')}_${
       user.id
     }`;
     session.dialogData.statusList[taskId] = session.dialogData.status;
     // Check for end of form
-    if (session.dialogData.questionIndex >= questions.length) {
+    if (session.dialogData.questionIndex < questions.length) {
       // Return completed form
-      session.dialogData.status = {};
-      session.dialogData.questionIndex = 0;
-      session.dialogData.userIndex++;
-    }
-    if (session.dialogData.userIndex >= users.length) {
-      session.endDialogWithResult({ response: session.dialogData.statusList });
-    } else {
+      // session.dialogData.status = {};
+      // session.dialogData.questionIndex = 0;
       session.replaceDialog('statusDialog', session.dialogData);
+    } else {
+      session.send('Conclusion');
+      console.log(JSON.stringify(session.dialogData.statusList));
+      var tasksRef = dB.ref().child('tasks');
+      var obj = {};
+      var taskId = `${projectId}_${moment(new Date()).format('DD-MM-YYYY')}_${
+        user.id
+      }`;
+      obj = {
+        yTask: session.dialogData.statusList[taskId].yTask,
+        tTask: session.dialogData.statusList[taskId].tTask,
+        blocker: session.dialogData.statusList[taskId].blocker
+      };
+      tasksRef.child(taskId).set(obj);
+      session.send(
+        `${user.name}: <br> ${
+          session.dialogData.statusList[taskId].yTask
+        } <br> ${session.dialogData.statusList[taskId].tTask} <br> ${
+          session.dialogData.statusList[taskId].blocker
+        }`
+      );
+      session.endDialog();
     }
   }
 ]);
